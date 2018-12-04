0c72a7e50f8d3d907fd6a8d99fc41be29bd0c8a8
MadBoyScout <45595996+MadBoyScout@users.noreply.github.com>
2018-12-04 13:36:48 +0100

    Create Population.java

---
+++ Population.java
@@ -0,0 +1,34 @@
+public class Population{
+	public Population(double omega){
+
+
+	}
+
+	public void add(Individual i){
+
+
+	}
+
+	public void remove(Individual i){
+
+	}
+
+	public int size(){
+	return noget length af det set vi ender med at vælge	
+	}
+
+	public void epidemic(){
+
+	}
+
+	public double fitness(Individual i){
+		double fitness = (Math.pow((omega+(costminimum/cost(p))),2))/(1+2*omega);
+		return fitness
+	}
+
+	public City[] bestPath(){
+		return 
+	}
+
+	 
+}
